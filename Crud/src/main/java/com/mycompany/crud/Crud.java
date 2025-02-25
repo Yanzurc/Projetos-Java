d81b6c22238d6ae0648365ae25c174452a8c9746
Yan Cruz <98358800+Yanzurc@users.noreply.github.com>
2025-02-25 13:27:55 -0300

    Add files via upload

---
+++ Crud/src/main/java/com/mycompany/crud/Crud.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crud;
+
+/**
+ *
+ * @author yuram
+ */
+public class Crud {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
